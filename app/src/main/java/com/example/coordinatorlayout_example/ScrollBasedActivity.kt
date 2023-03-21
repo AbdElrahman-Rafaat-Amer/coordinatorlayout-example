58e38916f04696bd7c70fc707e200ff81db456ce
AbdElRahman Rafaat Amer <abdelrahman.raafaat@nweave.com>
2023-03-21 20:17:34 +0200

    bjvhjbhkj

--- app/src/main/java/com/example/coordinatorlayout_example/ScrollBasedActivity.kt
+++ app/src/main/java/com/example/coordinatorlayout_example/ScrollBasedActivity.kt
@@ -10,10 +10,7 @@ class ScrollBasedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_based)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        toolbar_layout.title = title
+
         // You can add the code for your FAB also.
     }
 
